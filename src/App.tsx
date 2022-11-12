dab7e8e24e4bac98b06d49c0d0e65ab3d1fddb12
Doanh Duc <doanhthangduc2802@gmail.com>
2022-11-12 17:32:18 +0700

    create footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./shared/Global.styled";
 import Header from "./components/header/Header";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LifeStyle from "./components/pages/LifeStyle";
 import Music from "./components/pages/Music";
 import Photodiary from "./components/pages/Photodiary";
 import Travel from "./components/pages/Travel";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LifeStyle />} />
-        <Route path="/" element={<Music />} />
-        <Route path="/" element={<Photodiary />} />
-        <Route path="/" element={<Travel />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/photodiary" element={<Photodiary />} />
+        <Route path="/travel" element={<Travel />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
